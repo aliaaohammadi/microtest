d540cddee50bc22dd1e76a6030e0595c7a83f897
Ali Mohammadi <amoh@chalmers.se>
2026-02-22 19:05:05 +0100

    learning stuff

--- scripts/run_core_demo.py
+++ scripts/run_core_demo.py
@@ -1,29 +1,15 @@
 from microtest.core import add, multiply
-# alternatively:
-# from microtest import add, multiply
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from microtest.core import entropy
+df = pd.read_csv(r"C:\\Users\\amoh\\Desktop\\microtox\\data\\processed\\treadmill\\P001_L0_T1.csv")
 
 
-def main():
-    print("Running core demo script")
+gyro_x = df["IMU-1 Gyr.X"]
+entropy_value = entropy(gyro_x)
 
-    a = 5
-    b = 7
+print("Shannon Entropy:", entropy_value)
 
-    sum_result = add(a, b)
-    product_result = multiply(a, b)
-
-    print(f"Numbers: a={a}, b={b}")
-    print(f"Sum: {sum_result}")
-    print(f"Product: {product_result}")
-
-    values = [(1, 2), (3, 4), (10, 20)]
-
-    print("\nBatch operations:")
-    for x, y in values:
-        s = add(x, y)
-        p = multiply(x, y)
-        print(f"{x}, {y} → sum={s}, product={p}")
-
-
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
--- src/microtest/core.py
+++ src/microtest/core.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def add(a: float, b: float) -> float:
     return a + b
 
@@ -5,3 +8,19 @@ def add(a: float, b: float) -> float:
 def multiply(a: float, b: float) -> float:
     return a * b 
 
+def entropy(signal, n_bins=50):
+
+
+    signal = np.asarray(signal)
+    signal = signal[~np.isnan(signal)]
+    
+    # Create histogram (probability distribution)
+    hist, bin_edges = np.histogram(signal, bins=n_bins, density=True)
+    
+    # Remove zero probabilities
+    hist = hist[hist > 0]
+    
+    # Compute entropy
+    entropy = -np.sum(hist * np.log2(hist))
+    
+    return entropy
