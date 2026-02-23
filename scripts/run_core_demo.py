bee3ccefccf7c82893b561e357d6baa4d1dc1c90
Ali Mohammadi <amoh@chalmers.se>
2026-02-23 13:03:00 +0100

    done for now

--- scripts/run_core_demo.py
+++ scripts/run_core_demo.py
@@ -3,13 +3,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from microtest.core import entropy
+
+
 df = pd.read_csv(r"C:\\Users\\amoh\\Desktop\\microtox\\data\\processed\\treadmill\\P001_L0_T1.csv")
+
+
+
 
 
 gyro_x = df["IMU-1 Gyr.X"]
 entropy_value = entropy(gyro_x)
 
-print("Shannon Entropy:", entropy_value)
+print("Entropy:", entropy_value)
 
 # if __name__ == "__main__":
 #     main()
